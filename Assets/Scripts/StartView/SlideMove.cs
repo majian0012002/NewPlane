2501947e384825829ffe84544b5eeed87bb3d963
majian0012002 <majian0012002@sina.com>
2017-01-02 23:41:09 +0800

    第一次提交
    1. 构建Android环境并且与Unity集成。
    2. 搭建游戏环境。
    3. 开始游戏sence完成。

---
+++ Assets/Scripts/StartView/SlideMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideMove : MonoBehaviour {
+
+	public UIPlayTween pt;
+	int num;
+
+	// Use this for initialization
+	void Start () {
+		pt = GetComponent<UIPlayTween> ();
+		num = PlayerPrefs.GetInt ("LeveNum");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnButtonClick () {
+		Debug.Log ("Click");
+	}
+}
